5a05fd76f6a441bba4a1b28875e3ba45acec3751
CodeLaunch <livesiddharth@hotmail.com>
2015-11-22 22:07:31 +0530

    Crawlers code.

---
+++ Crawlers/CrawlerPlace.py
@@ -0,0 +1,79 @@
+from googleplaces import GooglePlaces, types, lang
+import MySQLdb
+
+def addtable2(Name,Address,website,Phone,Mobile,lattitude,longitude):
+    con = MySQLdb.connect(host='127.0.0.1', user='root', passwd='root', db='cater_db')
+    cur = con.cursor()
+    
+    cur.execute('INSERT INTO caters2(Name,Address,website,Phone,Mobile,lattitude,longitude) values (%s,%s,%s,%s,%s,%s,%s)',(Name,Address,website,Phone,Mobile,lattitude,longitude)) 
+    con.commit()
+    mid = cur.lastrowid
+    con.close()
+    return mid
+
+API_KEY = 'AIzaSyCtGoyJVBHQ_mG8NaXST6jiTvltUtmbCVU'
+google_places = GooglePlaces(API_KEY)
+
+# You may prefer to use the text_search API, instead.
+
+location=raw_input('Enter Location: ')
+
+Search=raw_input('Enter Search: ')
+
+query_result = google_places.nearby_search(
+        location=location, keyword=Search,
+        radius=30)
+
+if query_result.has_attributions:
+    print query_result.html_attributions
+index=1
+Results = []
+for place in query_result.places:
+    Result = []
+    # Returned places from a query are place summaries.
+    Name = ''
+    lattitude = ''
+    longitude = ''
+    website = ''
+    address = ''
+    phone = ''
+    mobile = ''
+    print 'Index:',index
+    #Result.append(index)
+    index=index+1
+    print 'Name:',place.name
+    Name = place.name
+    #Result.append(place.name)
+    print 'Geographic Location:',place.geo_location['lat'],place.geo_location['lng']
+    lattitude = place.geo_location['lat']
+    longitude = place.geo_location['lng']
+    #Result.append(place.geo_location['lat'])
+    #Result.append(place.geo_location['lng'])
+    #print place.reference
+
+    # The following method has to make a further API call.
+    place.get_details()
+
+    # A dict matching the JSON response from Google.
+    try:
+        print 'WebSite:',place.details['website']
+        website = place.details['website']
+        #Result.append(place.details['website'])
+    except Exception,e:
+        #Result.append(" ")
+        print `e`
+    try:
+        print 'Address:',place.details['vicinity']
+        address = place.details['vicinity'].replace(",", " ")
+        #Result.append(address)
+    except Exception,e:
+        #Result.append(" ")
+        print `e`        
+    print 'Type:',place.details['types'][0]
+    #Result.append(place.details['types'][0])
+    print place.local_phone_number
+    phone = place.local_phone_number
+    #Result.append(place.local_phone_number)
+    print place.international_phone_number
+    mobile = place.international_phone_number
+    addtable2(Name,address,website,phone,mobile,lattitude,longitude)
---
+++ Crawlers/Crawler_bookmyparty.py
@@ -0,0 +1,78 @@
+#URL http://bookmyparty.co.in/SearchList.aspx?q=Caterers&Page=1 25
+
+
+import requests
+import BeautifulSoup
+import re
+import MySQLdb
+
+
+def addtable1(Name,POC,Address,Phone,Mobile,Cuisines,map_address):
+    con = MySQLdb.connect(host='127.0.0.1', user='root', passwd='root', db='cater_db')
+    cur = con.cursor()
+    
+    cur.execute('INSERT INTO caters(Name,POC,Address,Phone,Mobile,Cuisines,map_address) values (%s,%s,%s,%s,%s,%s,%s)',(Name,POC,Address,Phone,Mobile,Cuisines,map_address)) 
+    con.commit()
+    mid = cur.lastrowid
+    con.close()
+    return mid
+
+def get_data(link):
+	try:
+		i = 1
+		while i<26:
+			link=link+str(i)
+			req=requests.get(link,headers=headers)
+			data=req.text
+			soup=BeautifulSoup.BeautifulSoup(''.join(data))
+			#category=[]
+			Cater_Name=''
+			Cater_POC = ''
+			Cater_address = ''
+			Cater_cuisines = ''
+			Cater_PhoneNo = ''
+			Cater_MobileNo = ''
+			Cater_map = ''
+			for div in soup.findAll('div',{'class':'minMicro'}):
+				h1 =  div.find('h1',{'class':'prpl'})
+				Cater_Name =  h1.text
+				p1 = div.find('p',{'style':'display:none;'})
+				Cater_POC = p1.text
+				try:
+					p2 =  div.find('p',{'class':'phn'})
+					Cater_PhoneNo =  p2.text
+				except Exception,e:
+					print str(e)
+				try:
+					p3 =  div.find('p',{'class':'mob'})
+					Cater_MobileNo =  p3.text
+				except Exception,e:
+					print str(e)
+				for bold in div.findAll('b'):
+					if bold.parent.name == 'p':
+						try:
+							if bold.text == 'Cuisines':
+								Cater_cuisines = bold.parent.text
+						except Exception,e:
+							print str(e)
+						try:
+							if bold.text == 'Address':
+								Cater_address = bold.parent.text
+								Cater_address = Cater_address.replace("Address: ","")
+								Cater_address = Cater_address.replace("|View Map","")
+								Cater_address = ' '.join(Cater_address.split())
+						except Exception,e:
+							print str(e)
+				Cater_map = div.find('a',{'class':'mp'})
+				Cater_map = Cater_map['href'] 
+				addtable1(Cater_Name,Cater_POC,Cater_address,Cater_PhoneNo,Cater_MobileNo,Cater_cuisines,Cater_map)	
+
+	except Exception,e:
+			print str(e)
+
+
+
+headers  ={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0'}
+link= 'http://bookmyparty.co.in/SearchList.aspx?q=Caterers&Page='
+
+get_data(link)
